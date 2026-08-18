894213fcb7c29dd8fb24c24b32de9123e4555d1d
Krtl_lab <krtlproject.id@gmail.com>
2026-08-18 13:49:19 +0700

    modified:   .open-next/.build/durable-objects/queue.js 	modified:   .open-next/assets/BUILD_ID 	renamed:    .open-next/assets/_next/static/OinE-eGv7PJhOJ0QN2HRM/_buildManifest.js -> .open-next/assets/_next/static/yUt5jN0g4fh_Y07NfOeEr/_buildManifest.js 	renamed:    .open-next/assets/_next/static/OinE-eGv7PJhOJ0QN2HRM/_ssgManifest.js -> .open-next/assets/_next/static/yUt5jN0g4fh_Y07NfOeEr/_ssgManifest.js 	renamed:    .open-next/cache/OinE-eGv7PJhOJ0QN2HRM/500.cache -> .open-next/cache/yUt5jN0g4fh_Y07NfOeEr/500.cache 	renamed:    .open-next/cache/OinE-eGv7PJhOJ0QN2HRM/_not-found.cache -> .open-next/cache/yUt5jN0g4fh_Y07NfOeEr/_not-found.cache 	renamed:    .open-next/cache/OinE-eGv7PJhOJ0QN2HRM/about.cache -> .open-next/cache/yUt5jN0g4fh_Y07NfOeEr/about.cache 	renamed:    .open-next/cache/OinE-eGv7PJhOJ0QN2HRM/admin.cache -> .open-next/cache/yUt5jN0g4fh_Y07NfOeEr/admin.cache 	renamed:    .open-next/cache/OinE-eGv7PJhOJ0QN2HRM/admin/changelog.cache -> .open-next/cache/yUt5jN0g4fh_Y07NfOeEr/admin/changelog.cache 	renamed:    .open-next/cache/OinE-eGv7PJhOJ0QN2HRM/admin/cloud.cache -> .open-next/cache/yUt5jN0g4fh_Y07NfOeEr/admin/cloud.cache 	renamed:    .open-next/cache/OinE-eGv7PJhOJ0QN2HRM/admin/document.cache -> .open-next/cache/yUt5jN0g4fh_Y07NfOeEr/admin/document.cache 	renamed:    .open-next/cache/OinE-eGv7PJhOJ0QN2HRM/admin/document/new.cache -> .open-next/cache/yUt5jN0g4fh_Y07NfOeEr/admin/document/new.cache 	renamed:    .open-next/cache/OinE-eGv7PJhOJ0QN2HRM/admin/orders.cache -> .open-next/cache/yUt5jN0g4fh_Y07NfOeEr/admin/orders.cache 	renamed:    .open-next/cache/OinE-eGv7PJhOJ0QN2HRM/admin/products.cache -> .open-next/cache/yUt5jN0g4fh_Y07NfOeEr/admin/products.cache 	renamed:    .open-next/cache/OinE-eGv7PJhOJ0QN2HRM/admin/products/new.cache -> .open-next/cache/yUt5jN0g4fh_Y07NfOeEr/admin/products/new.cache 	renamed:    .open-next/cache/OinE-eGv7PJhOJ0QN2HRM/admin/settings.cache -> .open-next/cache/yUt5jN0g4fh_Y07NfOeEr/admin/settings.cache 	renamed:    .open-next/cache/OinE-eGv7PJhOJ0QN2HRM/admin/tools.cache -> .open-next/cache/yUt5jN0g4fh_Y07NfOeEr/admin/tools.cache 	renamed:    .open-next/cache/OinE-eGv7PJhOJ0QN2HRM/blog.cache -> .open-next/cache/yUt5jN0g4fh_Y07NfOeEr/blog.cache 	renamed:    .open-next/cache/OinE-eGv7PJhOJ0QN2HRM/cart.cache -> .open-next/cache/yUt5jN0g4fh_Y07NfOeEr/cart.cache 	renamed:    .open-next/cache/OinE-eGv7PJhOJ0QN2HRM/help.cache -> .open-next/cache/yUt5jN0g4fh_Y07NfOeEr/help.cache 	renamed:    .open-next/cache/OinE-eGv7PJhOJ0QN2HRM/index.cache -> .open-next/cache/yUt5jN0g4fh_Y07NfOeEr/index.cache 	renamed:    .open-next/cache/OinE-eGv7PJhOJ0QN2HRM/login.cache -> .open-next/cache/yUt5jN0g4fh_Y07NfOeEr/login.cache 	renamed:    .open-next/cache/OinE-eGv7PJhOJ0QN2HRM/privacy-polish.cache -> .open-next/cache/yUt5jN0g4fh_Y07NfOeEr/privacy-polish.cache 	renamed:    .open-next/cache/OinE-eGv7PJhOJ0QN2HRM/products.cache -> .open-next/cache/yUt5jN0g4fh_Y07NfOeEr/products.cache 	modified:   .open-next/cloudflare/cache-assets-manifest.sql 	modified:   .open-next/cloudflare/init.js 	modified:   .open-next/dynamodb-provider/dynamodb-cache.json 	modified:   .open-next/middleware/handler.mjs 	modified:   .open-next/server-functions/default/handler.mjs 	modified:   .open-next/server-functions/default/handler.mjs.meta.json 	modified:   .open-next/server-functions/default/index.mjs

--- .open-next/.build/durable-objects/queue.js
+++ .open-next/.build/durable-objects/queue.js
@@ -156,7 +156,7 @@ var DOQueueHandler = class extends DurableObject {
         method: "HEAD",
         headers: {
           // This is defined during build
-          "x-prerender-revalidate": "87445b6fd489d228f1e3afa942298d7f",
+          "x-prerender-revalidate": "84292b372f11e7b2cb3497de10aa3106",
           "x-isr": "1"
         },
         // This one is kind of problematic, it will always show the wall time of the revalidation to `this.revalidationTimeout`
@@ -180,7 +180,7 @@ var DOQueueHandler = class extends DurableObject {
           "INSERT OR REPLACE INTO sync (id, lastSuccess, buildId) VALUES (?, unixepoch(), ?)",
           // We cannot use the deduplication id because it's not unique per route - every time a route is revalidated, the deduplication id is different.
           `${host}${url}`,
-          "OinE-eGv7PJhOJ0QN2HRM"
+          "yUt5jN0g4fh_Y07NfOeEr"
         );
       }
       this.routeInFailedState.delete(msg.MessageDeduplicationId);
@@ -232,7 +232,7 @@ var DOQueueHandler = class extends DurableObject {
     }
     this.routeInFailedState.set(msg.MessageDeduplicationId, updatedFailedState);
     if (!this.disableSQLite) {
-      this.sql.exec("INSERT OR REPLACE INTO failed_state (id, data, buildId) VALUES (?, ?, ?)", msg.MessageDeduplicationId, JSON.stringify(updatedFailedState), "OinE-eGv7PJhOJ0QN2HRM");
+      this.sql.exec("INSERT OR REPLACE INTO failed_state (id, data, buildId) VALUES (?, ?, ?)", msg.MessageDeduplicationId, JSON.stringify(updatedFailedState), "yUt5jN0g4fh_Y07NfOeEr");
     }
     await this.addAlarm();
   }
@@ -256,8 +256,8 @@ var DOQueueHandler = class extends DurableObject {
       return;
     this.sql.exec("CREATE TABLE IF NOT EXISTS failed_state (id TEXT PRIMARY KEY, data TEXT, buildId TEXT)");
     this.sql.exec("CREATE TABLE IF NOT EXISTS sync (id TEXT PRIMARY KEY, lastSuccess INTEGER, buildId TEXT)");
-    this.sql.exec("DELETE FROM failed_state WHERE buildId != ?", "OinE-eGv7PJhOJ0QN2HRM");
-    this.sql.exec("DELETE FROM sync WHERE buildId != ?", "OinE-eGv7PJhOJ0QN2HRM");
+    this.sql.exec("DELETE FROM failed_state WHERE buildId != ?", "yUt5jN0g4fh_Y07NfOeEr");
+    this.sql.exec("DELETE FROM sync WHERE buildId != ?", "yUt5jN0g4fh_Y07NfOeEr");
     const failedStateCursor = this.sql.exec("SELECT * FROM failed_state");
     for (const row of failedStateCursor) {
       this.routeInFailedState.set(row.id, JSON.parse(row.data));
